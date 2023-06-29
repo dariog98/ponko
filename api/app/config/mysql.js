f79975276df61cb66cb2545811ed9c62c70c8106
dariog98 <dariogoirad98@gmail.com>
2023-06-28 21:28:03 -0300

    Update

--- api/app/config/mysql.js
+++ api/app/config/mysql.js
@@ -2,12 +2,7 @@ import * as dotenv from 'dotenv'
 import { Sequelize } from "sequelize"
 
 dotenv.config()
-/*
-const hostname = "localhost"
-const database = "ponkoApp"
-const username = "root"
-const password = "12345"
-*/
+
 const hostname = process.env.DB_HOST
 const database = process.env.DB_NAME
 const username = process.env.DB_USER
